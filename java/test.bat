9edb752770a466e60888a203374179a633b89a8f
Tom Peer <tom@clik.com>
2025-06-03 14:41:38 +0100

    3 comment remove fix

--- java/test.bat
+++ java/test.bat
@@ -1,4 +1,4 @@
 javac -cp .;c:/dev/java_library/jsoup-1.20.1.jar;out/org/coldsoup/coldsoup.jar test/TestRemoveComments.java
 cd test
-java -cp .;c:/dev/java_library/jsoup-1.20.1.jar; out/org/coldsoup/coldsoup.jar; TestRemoveComments
+java -cp .;c:/dev/java_library/jsoup-1.20.1.jar;../out/org/coldsoup/coldsoup.jar; TestRemoveComments
 cd ..
